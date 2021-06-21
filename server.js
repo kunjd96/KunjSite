cf06a9e286d29ce260874a154ea96d45d6e8b5e0
kunjd96 <kunjd96@gmail.com>
2021-06-20 22:56:50 -0400

    firstcommit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({path:'./config/config.env'});
+
+const app = express();
+app.use(express.static(__dirname + '/dist/kunjsite'));
+
+const port = process.env.PORT || 5000;
+app.get('/*' , (req , res)=>{
+
+   res.sendFile(path.join(__dirname));
+
+})
+
+app.listen(port, console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`))
